fc89b7d3460b870b8198222d19f0b60936d7ff41
chrisburbank <chrisburbank@gmail.com>
2025-04-15 03:39:03 +0000

    fixed deploy

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import * as process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-        }
-    }
+    sourcemap: process.env.NODE_ENV !== 'production',
+    rollupOptions: {}
   },
   resolve: {
     alias: {
